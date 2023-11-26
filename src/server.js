a078fa219f25035bd45b0bafee6bb4a3c25adc4f
chobolevel <rodaka123@naver.com>
2023-11-26 17:43:43 +0900

    Refactor: apply prettier convention

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
-import express from "express";
-import morgan from "morgan";
+import express from "express"
+import morgan from "morgan"
 
-const PORT = 4000;
+const PORT = 4000
 
-const app = express();
-const logger = morgan("dev");
+const app = express()
+const logger = morgan("dev")
 
 const home = (req, res) => {
-  return res.send("hello");
-};
+  return res.send("hello")
+}
 const login = (req, res) => {
-  return res.send("login");
-};
+  return res.send("login")
+}
 
-app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+app.use(logger)
+app.get("/", home)
+app.get("/login", login)
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
-});
+  console.log(`Server listening on port http://localhost:${PORT}`)
+})
