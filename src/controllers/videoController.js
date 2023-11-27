ff211712cf6cee776034c1aca23bd2d2476ed204
chobolevel <rodaka123@naver.com>
2023-11-27 14:57:41 +0900

    #4.3 Exports

---
+++ src/controllers/videoController.js
@@ -0,0 +1,9 @@
+export const trending = (req, res) => {
+  return res.send("Home Page Videos")
+}
+export const watch = (req, res) => {
+  return res.send("Watch Video")
+}
+export const edit = (req, res) => {
+  return res.send("Edit Video")
+}
