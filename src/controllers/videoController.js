438eef49baf3060959bb0facf6d5827d919227e9
chobolevel <rodaka123@naver.com>
2023-11-28 18:10:45 +0900

    #5.0 Rendering HTML

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 export const trending = (req, res) => {
-  return res.send("Home Page Videos")
+  return res.send("<h1>Hello1</h1>")
 }
 export const see = (req, res) => {
   console.log(req.params)
