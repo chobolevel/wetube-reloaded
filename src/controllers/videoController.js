e2616ef56d854144d991ed6e20bfc648d2951db4
chobolevel <rodaka123@naver.com>
2023-11-29 19:12:10 +0900

    #6.11 Our FIrst Query

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,55 +1,22 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 Minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 Minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 Minutes ago",
-    views: 59,
-    id: 3,
-  },
-]
-export const trending = (req, res) => {
+import Video from "../models/Video"
+
+export const home = (req, res) => {
+  Video.find({}, (err, videos) => {})
   return res.render("home", {
     pageTitle: "Home",
-    videos,
   })
 }
 export const watch = (req, res) => {
   const { id } = req.params
-  const video = videos.find((v) => v.id === Number(id))
-  return res.render("watch", { pageTitle: video.title, video })
+  return res.render("watch", { pageTitle: "watch" })
 }
 export const getEdit = (req, res) => {
   const { id } = req.params
-  const video = videos.find((v) => v.id === Number(id))
-  return res.render("edit", { pageTitle: `Edit ${video.title}`, video })
+  return res.render("edit", { pageTitle: "edit" })
 }
 export const postEdit = (req, res) => {
   const { id } = req.params
   const { title } = req.body
-  videos.map((v) => {
-    if (v.id === Number(id)) {
-      return (v.title = title)
-    } else {
-      return v
-    }
-  })
   return res.redirect(`/videos/${id}`)
 }
 export const getUpload = (req, res) => {
@@ -57,14 +24,5 @@ export const getUpload = (req, res) => {
 }
 export const postUpload = (req, res) => {
   const { title } = req.body
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  }
-  videos.push(newVideo)
   return res.redirect("/")
 }
